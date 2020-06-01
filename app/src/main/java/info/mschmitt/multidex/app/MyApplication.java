8fa0301c18abae501666d433b7cd20987b2d937e
Matthias Schmitt <m6s@mschmitt.info>
2020-06-01 19:25:57 +0200

    Workaround

--- app/src/main/java/info/mschmitt/multidex/app/MyApplication.java
+++ app/src/main/java/info/mschmitt/multidex/app/MyApplication.java
@@ -1,5 +1,7 @@
 package info.mschmitt.multidex.app;
 
+import android.content.Context;
+
 import androidx.collection.ArraySet;
 import androidx.multidex.MultiDexApplication;
 
@@ -9,6 +11,19 @@ import com.google.gson.Gson;
  * @author Matthias Schmitt
  */
 public class MyApplication extends MultiDexApplication {
-//    public Gson gson = new Gson();
-    public ArraySet<Integer> set = new ArraySet<>();
+    public Gson gson;
+    public ArraySet<Integer> set;
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        gson = new Gson(); // This works without another indirection
+        Indirections.initialize(this);
+    }
+
+    private static class Indirections {
+        static void initialize(MyApplication application) {
+            application.set = new ArraySet<>(); // This only works embedded inside a different class
+        }
+    }
 }
